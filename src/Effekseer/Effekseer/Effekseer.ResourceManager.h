b0dcccb82de4bb3d08c559ebb8f983b1b6ffaacd
Danil Dubrovsky <forwot163@gmail.com>
2022-03-31 18:34:09 +0300

    Fixed cached resources reloading

--- src/Effekseer/Effekseer/Effekseer.ResourceManager.h
+++ src/Effekseer/Effekseer/Effekseer.ResourceManager.h
@@ -171,7 +171,7 @@ private:
 		{
 			if (loader != nullptr && resource != nullptr)
 			{
-				if (resource->GetPath().empty())
+				if (!resource->GetPath().empty())
 				{
 					auto it = cached.find(resource->GetPath());
 					if (it != cached.end())
